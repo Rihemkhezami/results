5425173e99255b1c8276cdd74ea398379e2fe8c4
rihem khezami <rihemkhezami5@gmail.com>
2024-07-29 01:34:23 +0100

    appication nextJS

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+<div>
+        <Typography variant="h2" >
+          Results
+        </Typography>
+</div>
+  );
+}
